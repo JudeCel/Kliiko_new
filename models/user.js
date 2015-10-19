6dcaace0075880108969adb12f1f2801614911e3
DainisL <dainis186@gmail.com>
2015-10-19 22:41:56 +0300

    added user model

---
+++ models/user.js
@@ -0,0 +1,45 @@
+"use strict";
+var bcrypt = require('bcrypt');
+
+module.exports = (Sequelize, DataTypes) => {
+  var User = Sequelize.define('User', {
+    first_name: {type: DataTypes.INTEGER, allowNull: false},
+    last_name: {type: DataTypes.INTEGER, allowNull: false},
+    email: {type: DataTypes.STRING, allowNull: false, unique: true},
+    encrypted_password:  {type : DataTypes.STRING, allowNull: false},
+    password: {
+      type: DataTypes.VIRTUAL,
+      set: function(val) {
+        this.setDataValue('password', val);
+        if (val) {
+          this.setDataValue('encrypted_password', bcrypt.hashSync(val, 10))
+        };
+      },
+      validate: {
+        isLongEnough: (val) => {
+          if (val.length < 7) {
+            throw new Error("Please choose a longer password")
+          }
+        }
+      }
+    },
+    reset_password_token: {type : DataTypes.INTEGER, allowNull: true},
+    reset_password_sent_at: {type : DataTypes.DATE, allowNull: true},
+    current_sign_in_ip: {type : DataTypes.STRING, allowNull: true},
+    last_sign_in_ip: {type : DataTypes.STRING, allowNull: true},
+    created_at: {type: DataTypes.DATE, allowNull: false, defaultValue: DataTypes.NOW},
+    updated_at: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: DataTypes.NOW
+    },
+    promo_code: {type: DataTypes.INTEGER, allowNull: true}
+  }, { hooks: {
+        beforeValidate: (user, options) => {
+          user.updated_at = new Date();
+        }
+      }
+    });
+
+  return User;
+};
---
+++ test/models/user.js
@@ -0,0 +1,27 @@
+"use strict";
+var assert = require('assert');
+var User  = require('./../../models').User;
+
+describe('User', () => {
+  describe('set encrypte password',  ()=>  {
+    before((done) => {
+      User.sync({ force: true }).then(() => {
+        done();
+      });
+    });
+
+
+    it('set encrypte password', (done) =>  {
+      let attrs = {
+        first_name: "Dainis",
+        last_name: "Lapins",
+        password: "cool_password",
+        email: "dainis@gmail.com"
+      }
+      User.build(attrs).validate().done(function(errors, user) {
+        assert.equal(errors, undefined);
+        done()
+      });
+    });
+  });
+});
