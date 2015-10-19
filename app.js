f5ea8229f56d73101c85b99934c5269dc106b663
DainisL <dainis186@gmail.com>
2015-10-19 14:15:35 +0300

    use ejs

--- app.js
+++ app.js
@@ -24,7 +24,8 @@ app.use(session({
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'ejs');
+app.set('view options', { layout:'layout.ejs' });
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
