9c9eac3bb6ac79dd15c4124fcb5433f82f91d703
DainisL <dainis186@gmail.com>
2015-10-29 15:14:28 +0200

    tidy up app.js
    Added middleware for passport

--- app.js
+++ app.js
@@ -8,14 +8,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var passport = require('passport');
-
-var usersRepo = require('./repositories/users.js');
-var socialProfileRepo = require('./repositories/socialProfile.js');
-var User  = usersRepo.user;
-var LocalStrategy = require('passport-local').Strategy;
-var FacebookStrategy = require('passport-facebook').Strategy;
-
+var passport = require('./middleware/passport')
 var app = express();
 
 // view engine setup
@@ -36,53 +29,6 @@ app.use(session({
     secret: config.get("sessionSecret"),
     resave: true, saveUninitialized: false
 }));
-
-passport.use(new LocalStrategy({
-  usernameField: 'email',
-  passwordField: 'password'
-  },
-  function(username, password, done) {
-    usersRepo.comparePassword(username, password, function(failed, result) {
-      if (failed) {
-        return done(null, false);
-      }else{
-        return done(null, result);
-      }
-    });
-  }
-));
-
-passport.use(new FacebookStrategy({
-    clientID: config.get("facebookclientID") ,
-    clientSecret: config.get("facebookClientSecret") ,
-    callbackURL: config.get("facebookCallbackURL"),
-    passReqToCallback : true,
-    profileFields: ['id', 'emails', 'name']
-
-  },
-  function(req, accessToken, refreshToken, profile, done) {
-    socialProfileRepo.findOrCreateFacebook(profile, function(error, result) {
-      socialProfileRepo.socialProfile.find({where: {id: result.id }, include: [ User ]}).done(function(sp) {
-        done(null, sp.User);
-      })
-    })
-  }
-));
-
-passport.serializeUser(function(user, done) {
-  done(null, {id: user.id, email: user.email, accountName: user.accountName});
-});
-
-passport.deserializeUser(function(userObject, done) {
-  User.find({attributes: ['email', 'accountName', 'id'], where: {id: userObject.id}}).done(function(result){
-    if (result) {
-      done(null, {id: result.id, email: result.email, accountName: result.accountName});
-    }else{
-      done("not found", null);
-    };
-  });
-});
-
 
 app.use(passport.initialize());
 app.use(passport.session());
@@ -120,6 +66,5 @@ app.use(function(err, req, res, next) {
     error: {}
   });
 });
-
 
 module.exports = app;
