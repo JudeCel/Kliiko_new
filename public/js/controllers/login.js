8f07026c67cac5f5da5498f26d2987bb05d3aca2
adsitdikovi <aleksejs.sitdikovs@gmail.com>
2015-10-29 11:35:44 +0200

    login page design is done

---
+++ public/js/controllers/login.js
@@ -0,0 +1,8 @@
+$('.input_class_checkbox').each(function(){
+    $(this).hide().after('<div class="class_checkbox" />');
+
+});
+
+$('.class_checkbox').on('click',function(){
+    $(this).toggleClass('checked').prev().prop('checked',$(this).is('.checked'))
+});
