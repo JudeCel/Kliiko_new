0b274ebb89a9897e308f1445c8331265ea7f692b
DainisL <dainis186@gmail.com>
2015-11-03 11:17:29 +0200

    change session expiry time
    rememberMeExpiryTime = 30 days
    standardExpiryTime = 1 day

--- middleware/session.js
+++ middleware/session.js
@@ -1,13 +1,15 @@
 "use strict";
 function rememberMe(req, done) {
+  let rememberMeExpiryTime = 3600 * 720 * 1000 // ~30 days
+  let standardExpiryTime = 300 * 320 * 1000 // ~1 day
+  req.session.cookie.expires = true;
+
   if (!!req.body.rememberMe) {
-    req.session.cookie.expires = false;
-    req.session.cookie.maxAge = 3600 * 720 * 1000; //30 days
+    req.session.cookie.maxAge = rememberMeExpiryTime;
     req.session.rememberMe = true;
   }else{
     req.session.rememberMe = false;
-    req.session.cookie.expires = false;
-    req.session.cookie.maxAge = 32 * 120 * 1000;// 1 hour
+    req.session.cookie.maxAge = standardExpiryTime;
   }
   done(null, true)
 }
