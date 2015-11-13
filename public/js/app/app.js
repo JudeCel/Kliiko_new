d19482a9266ca9f4b564c3fda1fbefa7c1daeabb
adsitdikovi <aleksejs.sitdikovs@gmail.com>
2015-11-13 11:27:04 +0200

    added structure for angular

---
+++ public/js/app/app.js
@@ -0,0 +1,16 @@
+/**
+ * Created by alex.sitdikov on 13.11.2015.
+ */
+'use strict';
+
+/* App Module */
+
+var kliiko = angular.module('Kliiko', [
+
+    //'kliikoFilters',
+    'kliikoServices',
+
+    //controllers
+    'dashboardCtrl'
+
+]);
--- public/js/controllers/dashboardCtrl.js
+++ public/js/controllers/dashboardCtrl.js
@@ -1,3 +1,16 @@
 /**
  * Created by alex.sitdikov on 13.11.2015.
  */
+'use strict';
+
+/* Controllers */
+
+angular.module('Kliiko', []).controller('dashboardCtrl', ['$scope', '$routeParams',
+    function($scope, $routeParams) {
+console.log('asdasd');
+        //TODO write here your code for this controller
+
+        $scope.alex = 10;
+
+
+    }]);
