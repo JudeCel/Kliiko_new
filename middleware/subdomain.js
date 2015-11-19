45743527b951b8a5d5e5975cbbb336421eb938aa
DainisL <dainis186@gmail.com>
2015-11-19 15:56:25 +0200

    use last subdomain only

--- middleware/subdomain.js
+++ middleware/subdomain.js
@@ -4,7 +4,7 @@ var config = require('config');
 var Account  = models.Account;
 
 module.exports = function(req, res, next) {
-  let subdomain = req.subdomains[0]
+  let subdomain = req.subdomains[req.subdomains.length - 1]// last element from array
   if (config.get("server")["baseSubdomain"] !== subdomain ) {
     Account.find({attributes: ['name'], where: { name: subdomain } }).then(function (result) {
       if(result){
