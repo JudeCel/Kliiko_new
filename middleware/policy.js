22a03b2deea5428be32453c173e2e1c289886840
DainisL <dainis186@gmail.com>
2015-11-19 11:30:41 +0200

    added policy file

---
+++ middleware/policy.js
@@ -0,0 +1,14 @@
+"use strict";
+var _ = require('lodash');
+
+var roles = [
+  "admin", "accountManager", "facilitator", "observer", "participant"]
+]
+
+function authorized(req, res, next) {
+
+}
+
+models.exports = {
+  authorized: authorized
+}
