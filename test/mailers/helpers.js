3681c2d2ff4bbba8344976175c3e2bd8b3adce67
Vladimirs Kacs <VladimirsKacs@gmail.com>
2015-11-26 20:57:54 +0200

    added more tests for mailers

--- test/mailers/helpers.js
+++ test/mailers/helpers.js
@@ -1,5 +1,5 @@
 "use strict";
-var assert = require('assert');
+var assert = require("chai").assert;
 var expect = require("chai").expect;
 var config = require('config');
 var helpers = require('./../../mailers/helpers.js');
@@ -18,4 +18,48 @@ describe('Mailer helpers', () => {
       });
     });
   });
+
+  describe('createTransport ', () => {
+    describe('success ', () => {
+      it('creates a fake transport for testing', (done) =>  {
+
+        let fakeTrans = helpers.createTransport();
+        let fakeData= {datum1:"aString", datum2:2, datum3:{iAmAnObject:true}};
+        fakeTrans.sendMail(fakeData,function(response){
+          assert.equal(response.data,fakeData);
+          done();
+        });
+
+      });
+    });
+  });
+
+  describe('renderMailTemplate ', () => {
+    describe('success ', () => {
+      it('renders a template into a string', (done) =>  {
+
+        let filename="testTemplate";
+        let params={variable:"e61096212b3a73267294192100f7c3b3"}
+        helpers.renderMailTemplate(filename, params,function(err,response){
+          assert.isNull(err);
+          assert.include(response,"This is a paragraph");
+          assert.include(response,params.variable);
+          done();
+        });
+      });
+    });
+
+      describe('no template ', () => {
+        it('returns an error', (done) =>  {
+
+          let filename="noTemplate";
+          let params={variable:"e61096212b3a73267294192100f7c3b3"}
+          helpers.renderMailTemplate(filename, params,function(err,response){
+            assert.isNotNull(err);
+            done();
+          });
+        });
+    });
+  });
+
 });
--- test/mailers/users.js
+++ test/mailers/users.js
@@ -18,4 +18,51 @@ describe('Mailer Users', () => {
       });
     });
   });
+
+  describe('sendPasswordChangedSuccess ', () => {
+    describe('success ', () => {
+      var email = "testeEmail@gmail.com"
+      var params = {'email': email}
+      it('content', (done) =>  {
+        usersMailer.sendPasswordChangedSuccess(params, function(result) {
+          assert.include(result.data.html, "Your password changed successfully");
+          assert.include(result.data.to, email);
+          done();
+        });
+      });
+    });
+  });
+
+  describe('sendEmailConfirmationToken ', () => {
+    describe('success ', () => {
+      var email = "testeEmail@gmail.com"
+      var token= "56b429319232ba613b76749988de4555"
+      var params = {'email': email, "token":token}
+      it('content', (done) =>  {
+        usersMailer.sendEmailConfirmationToken(params, function(result) {
+          assert.include(result.data.html, "Please confirm your email");
+          assert.include(result.data.to, email);
+          assert.include(result.data.html, token);
+          done();
+        });
+      });
+    });
+  });
+
+  describe('sendResetPasswordToken ', () => {
+    describe('success ', () => {
+      var email = "testeEmail@gmail.com"
+      var token= "56b429319232ba613b76749988de4555"
+      var params = {'email': email, "token":token}
+      it('content', (done) =>  {
+        usersMailer.sendResetPasswordToken(params, function(result) {
+          assert.include(result.data.html, "A request was made to change your password");
+          assert.include(result.data.to, email);
+          assert.include(result.data.html, token);
+          done();
+        });
+      });
+    });
+  });
+
 });
