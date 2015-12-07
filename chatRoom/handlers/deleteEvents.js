2f64e578c9beeb8c5197799957f5225965565110
DainisL <dainis186@gmail.com>
2015-12-07 11:24:15 +0200

    fixed delete all events

--- chatRoom/handlers/deleteEvents.js
+++ chatRoom/handlers/deleteEvents.js
@@ -1,17 +1,13 @@
 "use strict";
 var _ = require('lodash');
-var deleteEvents = require("if-data").repositories.deleteEvents;
-
-var ifData = require('if-data'), db = ifData.db;
 var webFaultHelper = require('../helpers/webFaultHelper.js');
-var Q = require('q');
 var joi = require('joi');
 
 var models = require("./../../models");
+var Event = models.Event;
 
 module.exports.validate = function (req, resCb) {
 	var err = joi.validate(req.params, { topicId: joi.number().required() });
-	var err = joi.validate({}, { topicId: joi.number().required() });
 
 	if (err.error)
 		return resCb(webFaultHelper.getValidationFault(err.error));
@@ -20,19 +16,16 @@ module.exports.validate = function (req, resCb) {
 };
 
 module.exports.run = function (req, resCb, errCb) {
-
-	let sql = "UPDATE events \
-		SET deleted = NOW() \
-		WHERE deleted IS NULL \
-		AND (tag = 0 OR tag = 16) \
-		AND cmd IN ('shareresource', 'object') \
-		AND topicId = ?";
-
-
-		models.sequelize.query(sql,
-			{ replacements: [req.params.topicId],
-				type: models.sequelize.QueryTypes.UPDATE} ).then(function(result) {
-				resCb.send();
-	  }).catch(function(err) {
-			errCb(webFaultHelper.getFault(err));
-		});};
+	Event.destroy({
+		where: {
+			topicId: req.params.topicId,
+			tag: { $in: [0, 16] },
+			cmd: { $in: ['shareresource', 'object'] }
+		}
+	})
+	.then(function(result) {
+		resCb.send();
+	}).catch(function(err) {
+		errCb(webFaultHelper.getFault(err));
+	});
+};
