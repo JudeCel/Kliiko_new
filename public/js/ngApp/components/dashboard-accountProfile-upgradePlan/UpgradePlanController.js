55c133f48c3c251f77bb0c0ac4251772e2b0f6ee
Pavel Karlovich <p.karlovich@gmail.com>
2015-12-16 17:44:20 +0200

    [#] clean up

--- public/js/ngApp/components/dashboard-accountProfile-upgradePlan/UpgradePlanController.js
+++ public/js/ngApp/components/dashboard-accountProfile-upgradePlan/UpgradePlanController.js
@@ -1,227 +1,243 @@
 (function () {
-    'use strict';
+  'use strict';
 
-    angular.
-    module('KliikoApp').
-    controller('UpgradePlanController', UpgradePlanController);
+  angular.
+  module('KliikoApp').
+  controller('UpgradePlanController', UpgradePlanController);
 
-    UpgradePlanController.$inject = ['dbg', 'domServices', '$state', '$stateParams','upgradePlanServices', '$scope', '$rootScope', 'user'];
-    function UpgradePlanController(dbg, domServices, $state,$stateParams, upgradePlanServices, $scope, $rootScope, user) {
-        dbg.log2('#UpgradePlanController  started');
-        var vm = this;
+  UpgradePlanController.$inject = ['dbg', 'domServices', '$state', '$stateParams', 'upgradePlanServices', '$scope', '$rootScope', 'user'];
+  function UpgradePlanController(dbg, domServices, $state, $stateParams, upgradePlanServices, $scope, $rootScope, user) {
+    dbg.log2('#UpgradePlanController  started');
+    var vm = this;
 
-        var modalTplPath = 'js/ngApp/components/dashboard-accountProfile-upgradePlan/tpls/';
-        var selectedPaymentMethod;
+    var modalTplPath = 'js/ngApp/components/dashboard-accountProfile-upgradePlan/tpls/';
+    var selectedPaymentMethod;
 
-        // set first step for 5 steps checkout process
-        $stateParams.planUpgradeStep = 1;
-        vm.currentStep = $stateParams.planUpgradeStep;
-        vm.$state = $state;
-        vm.modContentBlock = {selectedPlanDetails:true};
-        vm.updateBtn = 'Update';
-        vm.upgradeDuration = 1;
+    // set first step for 5 steps checkout process
+    $stateParams.planUpgradeStep = 1;
 
-        vm.paymentDetails = {
-            payPal: {
-                selected: false,
-                tos: false,
-            },
-            creditCard: {
-                number: null,
-                selected: false,
-                tos: false,
-                numberChanged:cardNumberChanged
-            },
+    vm.currentStep = $stateParams.planUpgradeStep;
+    vm.$state = $state;
+    vm.modContentBlock = {selectedPlanDetails: true};
+    vm.updateBtn = 'Update';
+    vm.upgradeDuration = 1;
+    vm.expDates = {
+      years: upgradePlanServices.getYearsArray(),
+      days: upgradePlanServices.getDaysArray()
+    };
 
-            changePaymentMethodTo: changePaymentMethodTo
-        };
+    vm.paymentDetails = {
+      payPal: {
+        selected: false,
+        tos: false,
+      },
+      creditCard: {
+        number: null,
+        selected: false,
+        tos: false,
+        numberChanged: cardNumberChanged
+      },
 
-        vm.stepsClassIsActive = stepsClassIsActive;
-        vm.stepsClassIsDone = stepsClassIsDone;
-        vm.openPlanDetailsModal = openPlanDetailsModal;
-        vm.upgradeToPlan = upgradeToPlan;
-        vm.applyCountryAndCurrency = applyCountryAndCurrency;
-        //vm.getUserData = getUserData;
-        vm.updateUserData = updateUserData;
-        vm.goToStep = goToStep;
+      changePaymentMethodTo: changePaymentMethodTo
+    };
 
-        init();
+    vm.stepsClassIsActive = stepsClassIsActive;
+    vm.stepsClassIsDone = stepsClassIsDone;
+    vm.openPlanDetailsModal = openPlanDetailsModal;
+    vm.upgradeToPlan = upgradeToPlan;
+    vm.applyCountryAndCurrency = applyCountryAndCurrency;
+    //vm.getUserData = getUserData;
+    vm.updateUserData = updateUserData;
+    vm.goToStep = goToStep;
 
-        function init() {
-            // get all plans details
-            upgradePlanServices.getPlans().then(function(res) { vm.plans = res });
+    init();
 
-            // get countries and currencies with exchange rates
-            upgradePlanServices.getAllCountriesList().then(function(res) {
-                dbg.log2('#UpgradePlanController > getAllCountriesList > res ', res);
-                vm.countries = res.all;
+    function init() {
+      // get all plans details
+      upgradePlanServices.getPlans().then(function (res) {
+        vm.plans = res
+      });
 
-                // then go and get currencies and exchange rates
-                dbg.log2('#UpgradePlanController > getAllCurrenciesList > call');
-                upgradePlanServices.getAllCurrenciesList().then(function(res) {
-                    vm.currencies = res;
-                    dbg.log2('#UpgradePlanController > getAllCurrenciesList > res ', res);
-                });
+      // get countries and currencies with exchange rates
+      upgradePlanServices.getAllCountriesList().then(function (res) {
+        dbg.log2('#UpgradePlanController > getAllCountriesList > res ', res);
+        vm.countries = res.all;
 
-            });
+        // then go and get currencies and exchange rates
+        dbg.log2('#UpgradePlanController > getAllCurrenciesList > call');
+        upgradePlanServices.getAllCurrenciesList().then(function (res) {
+          vm.currencies = res;
+          dbg.log2('#UpgradePlanController > getAllCurrenciesList > res ', res);
+        });
 
-            user.getUserData().then(function(res) {
-                vm.userData = res;
-            });
-        }
+      });
 
-        /**
-         * Helpers for ng-class
-         * @param step {int || char}
-         * @returns {boolean}
-         */
-        function stepsClassIsActive(step) { return (vm.currentStep == step); }
-        function stepsClassIsDone(step) {  return (vm.currentStep > step); }
+      user.getUserData().then(function (res) {
+        vm.userData = res;
+      });
+    }
 
+    /**
+     * Helpers for ng-class
+     * @param step {int || char}
+     * @returns {boolean}
+     */
+    function stepsClassIsActive(step) {
+      return (vm.currentStep == step);
+    }
 
-        /**
-         * Open plan upgrade modal with particular plan information
-         * @param plan {string}
-         */
-        function openPlanDetailsModal(plan) {
-            vm.currentPlan = plan;
-            vm.currentPlanModalContentTpl = modalTplPath + vm.currentPlan+'-plan.tpl.html';
-
-            domServices.modal('plansModal');
-        }
-
-        /**
-         * Proceed with selected plan upgrade steps
-         * @param plan {string}
-         */
-        function upgradeToPlan(plan) {
-            dbg.log('#UpgradePlanController > Upgrade to plan >', plan);
-
-            vm.selectedPlanName = plan;
-
-            domServices.modal('plansModal', 'close');
+    function stepsClassIsDone(step) {
+      return (vm.currentStep > step);
+    }
 
 
-            dbg.log2('#UpgradePlanController > getAllCountriesList > call');
-            upgradePlanServices.getAllCountriesList().then(function(res) {
-                dbg.log2('#UpgradePlanController > getAllCountriesList > res ', res);
-                vm.countries = res.all;
-                vm.selectedCountry = vm.countries[13];
-                domServices.modal('countryAndCurrencySelect');
+    /**
+     * Open plan upgrade modal with particular plan information
+     * @param plan {string}
+     */
+    function openPlanDetailsModal(plan) {
+      vm.currentPlan = plan;
+      vm.currentPlanModalContentTpl = modalTplPath + vm.currentPlan + '-plan.tpl.html';
+
+      domServices.modal('plansModal');
+    }
+
+    /**
+     * Proceed with selected plan upgrade steps
+     * @param plan {string}
+     */
+    function upgradeToPlan(plan) {
+      dbg.log('#UpgradePlanController > Upgrade to plan >', plan);
+
+      vm.selectedPlanName = plan;
+
+      domServices.modal('plansModal', 'close');
 
 
-                // then go and get currencies
-                dbg.log2('#UpgradePlanController > getAllCurrenciesList > call');
-                upgradePlanServices.getAllCurrenciesList().then(function(res) {
-                    //remove 'all' array and show only popular
-                    vm.currencies = res;
-                    vm.selectedCurrency = vm.currencies[vm.selectedCountry.currencies[0]];
-
-                    dbg.log2('#UpgradePlanController > getAllCurrenciesList > res ', res);
-
-                    $scope.$watch("up.selectedCountry",function(value){
-                        vm.selectedCurrency = vm.currencies[vm.selectedCountry.currencies[0]];
-                    });
-
-                });
-
-            });
+      dbg.log2('#UpgradePlanController > getAllCountriesList > call');
+      upgradePlanServices.getAllCountriesList().then(function (res) {
+        dbg.log2('#UpgradePlanController > getAllCountriesList > res ', res);
+        vm.countries = res.all;
+        vm.selectedCountry = vm.countries[13];
+        domServices.modal('countryAndCurrencySelect');
 
 
-        }
+        // then go and get currencies
+        dbg.log2('#UpgradePlanController > getAllCurrenciesList > call');
+        upgradePlanServices.getAllCurrenciesList().then(function (res) {
+          //remove 'all' array and show only popular
+          vm.currencies = res;
+          vm.selectedCurrency = vm.currencies[vm.selectedCountry.currencies[0]];
 
-        function applyCountryAndCurrency() {
-            dbg.log2('UpgradePlanController > Country and currency are selected > ', vm.selectedCountry, vm.selectedCurrency)
+          dbg.log2('#UpgradePlanController > getAllCurrenciesList > res ', res);
 
-            domServices.modal('countryAndCurrencySelect', 'close');
-            goToStep(2);
+          $scope.$watch("up.selectedCountry", function (value) {
+            vm.selectedCurrency = vm.currencies[vm.selectedCountry.currencies[0]];
+          });
 
-        }
+        });
 
-        function goToStep(step) {
-            if (!angular.isNumber(step)) {
-                if (step === 'back') step = vm.currentStep - 1;
-                if (step === 'next') step = vm.currentStep + 1;
-                if (step === 'submit') { step = 5; submitUpgrade() }
-            }
+      });
 
-            var valid = validateStep(step);
-
-            if (!valid) return;
-
-            $stateParams.planUpgradeStep = vm.currentStep = step;
-
-        }
-        function validateStep(step) {
-            if (step === 3) return validateStep2();
-
-            return true;
-
-            function validateStep2() {
-                // temporary solution
-                return true;
-            }
-        }
-
-        function updateUserData(data, form) {
-
-            //vm.updateBtn = 'Updating...';
-            user.updateUserData(data).then(function(res) {
-                vm.updateBtn = 'Updated';
-                setTimeout(function() {
-                    form.$setPristine();
-                    form.$setUntouched();
-                }, 2000);
-
-
-            });
-        }
-
-        function changePaymentMethodTo(type) {
-            if (type === 'payPal') {
-                vm.paymentDetails.payPal.selected = true;
-                vm.paymentDetails.payPal.tos = false;
-                vm.paymentDetails.creditCard.selected = false;
-            } else {
-                vm.paymentDetails.creditCard.selected = true;
-                vm.paymentDetails.creditCard.tos = false;
-                vm.paymentDetails.payPal.selected = false;
-            }
-
-            selectedPaymentMethod = type;
-        }
-
-        function cardNumberChanged() {
-            vm.paymentDetails.creditCard.number = upgradePlanServices.formatCreditCardNumber(vm.paymentDetails.creditCard.number);
-        }
-
-        function submitUpgrade() {
-            var planObject = {
-                plan: vm.plans[vm.selectedPlanName],
-                duration: vm.upgradeDuration
-            };
-
-            var paymentObject = {
-                paymentMethod: selectedPaymentMethod,
-                promocode: vm.promocode,
-                currency: vm.selectedCurrency.code,
-                totalPrice: vm.finalPrice
-            };
-
-            if (vm.paymentDetails.creditCard.selected) {
-                paymentObject.creditcardDetails = {
-                    cardHolderName: vm.paymentDetails.creditCard.holderName,
-                    cardNumber: vm.paymentDetails.creditCard.number.replace(/\D/g,''),
-                    expDate: vm.paymentDetails.creditCard.expDate,
-                    expYear: vm.paymentDetails.creditCard.expYear,
-                    cvv: vm.paymentDetails.creditCard.cvv
-                }
-            }
-
-            upgradePlanServices.submitUpgrade(planObject, paymentObject)
-        }
 
     }
+
+    function applyCountryAndCurrency() {
+      dbg.log2('UpgradePlanController > Country and currency are selected > ', vm.selectedCountry, vm.selectedCurrency)
+
+      domServices.modal('countryAndCurrencySelect', 'close');
+      goToStep(2);
+
+    }
+
+    function goToStep(step) {
+      if (!angular.isNumber(step)) {
+        if (step === 'back') step = vm.currentStep - 1;
+        if (step === 'next') step = vm.currentStep + 1;
+        if (step === 'submit') {
+          step = 5;
+          submitUpgrade()
+        }
+      }
+
+      var valid = validateStep(step);
+
+      if (!valid) return;
+
+      $stateParams.planUpgradeStep = vm.currentStep = step;
+
+    }
+
+    function validateStep(step) {
+      if (step === 3) return validateStep2();
+
+      return true;
+
+      function validateStep2() {
+        // temporary solution
+        return true;
+      }
+    }
+
+    function updateUserData(data, form) {
+
+      //vm.updateBtn = 'Updating...';
+      user.updateUserData(data).then(function (res) {
+        vm.updateBtn = 'Updated';
+        setTimeout(function () {
+          form.$setPristine();
+          form.$setUntouched();
+        }, 2000);
+
+
+      });
+    }
+
+    function changePaymentMethodTo(type) {
+      if (type === 'payPal') {
+        vm.paymentDetails.payPal.selected = true;
+        vm.paymentDetails.payPal.tos = false;
+        vm.paymentDetails.creditCard.selected = false;
+      } else {
+        vm.paymentDetails.creditCard.selected = true;
+        vm.paymentDetails.creditCard.tos = false;
+        vm.paymentDetails.payPal.selected = false;
+      }
+
+      selectedPaymentMethod = type;
+    }
+
+    function cardNumberChanged() {
+      vm.paymentDetails.creditCard.number = upgradePlanServices.formatCreditCardNumber(vm.paymentDetails.creditCard.number);
+    }
+
+    function submitUpgrade() {
+      var planObject = {
+        plan: vm.plans[vm.selectedPlanName],
+        duration: vm.upgradeDuration
+      };
+
+      var paymentObject = {
+        paymentMethod: selectedPaymentMethod,
+        promocode: vm.promocode,
+        currency: vm.selectedCurrency.code,
+        totalPrice: vm.finalPrice
+      };
+
+      if (vm.paymentDetails.creditCard.selected) {
+        paymentObject.creditcardDetails = {
+          cardHolderName: vm.paymentDetails.creditCard.holderName,
+          cardNumber: vm.paymentDetails.creditCard.number.replace(/\D/g, ''),
+          expDate: vm.paymentDetails.creditCard.expDate,
+          expYear: vm.paymentDetails.creditCard.expYear,
+          cvv: vm.paymentDetails.creditCard.cvv
+        }
+      }
+
+      upgradePlanServices.submitUpgrade(planObject, paymentObject)
+    }
+
+  }
 
 
 })();
--- public/js/ngApp/components/dashboard-accountProfile-upgradePlan/upgradePlanServices.js
+++ public/js/ngApp/components/dashboard-accountProfile-upgradePlan/upgradePlanServices.js
@@ -1,126 +1,139 @@
 (function () {
-    'use strict';
-    angular.module('KliikoApp').factory('upgradePlanServices', upgradePlanServices);
+  'use strict';
+  angular.module('KliikoApp').factory('upgradePlanServices', upgradePlanServices);
 
-    upgradePlanServices.$inject = ['$q','globalSettings', '$resource', 'dbg'];
-    function upgradePlanServices($q,globalSettings, $resource, dbg) {
+  upgradePlanServices.$inject = ['$q', 'globalSettings', '$resource', 'dbg'];
+  function upgradePlanServices($q, globalSettings, $resource, dbg) {
 
-        var upgradePlanRestApi = {
-            getAllCountries: $resource(globalSettings.restUrl+'/country-and-currency-data/countries', {}, {post: {method: 'POST'} }),
-            getAllCurrencies: $resource(globalSettings.restUrl+'/country-and-currency-data/currencies', {}, {post: {method: 'POST'} }),
-            getPlans: $resource(globalSettings.restUrl+'/plans', {}, {post: {method: 'POST'} }),
-            upgrade: $resource(globalSettings.restUrl+'/upgrade', {}, {post: {method: 'POST'} })
-        };
+    var upgradePlanRestApi = {
+      getAllCountries: $resource(globalSettings.restUrl + '/country-and-currency-data/countries', {}, {post: {method: 'POST'}}),
+      getAllCurrencies: $resource(globalSettings.restUrl + '/country-and-currency-data/currencies', {}, {post: {method: 'POST'}}),
+      getPlans: $resource(globalSettings.restUrl + '/plans', {}, {post: {method: 'POST'}}),
+      upgrade: $resource(globalSettings.restUrl + '/upgrade', {}, {post: {method: 'POST'}})
+    };
 
-        var cache = {};
-        var upServices = {};
+    var cache = {};
+    var upServices = {};
 
-        upServices.getAllCountriesList = getAllCountriesList;
-        upServices.getAllCurrenciesList = getAllCurrenciesList;
-        upServices.getPlans = getPlans;
-        upServices.formatCreditCardNumber = formatCreditCardNumber;
-        upServices.submitUpgrade = submitUpgrade;
+    upServices.getAllCountriesList = getAllCountriesList;
+    upServices.getAllCurrenciesList = getAllCurrenciesList;
+    upServices.getPlans = getPlans;
+    upServices.formatCreditCardNumber = formatCreditCardNumber;
+    upServices.submitUpgrade = submitUpgrade;
+    upServices.getYearsArray = getYearsArray;
+    upServices.getDaysArray = getDaysArray;
 
-        return upServices;
+    return upServices;
 
 
+    function getAllCountriesList() {
+      var deferred = $q.defer();
 
+      if (cache.allCountries) {
+        deferred.resolve(cache.allCountries);
+        dbg.log2('#upgradePlanServices > getAllCountriesList > return cached value');
+        return deferred.promise;
+      }
 
+      dbg.log2('#upgradePlanServices > getAllCountriesList > make rest call');
+      upgradePlanRestApi.getAllCountries.get({}, function (res) {
+        dbg.log2('#upgradePlanServices > getAllCountriesList > rest call responds');
+        deferred.resolve(res);
+        cache.allCountries = res;
+      });
 
-        function getAllCountriesList() {
-            var deferred = $q.defer();
+      return deferred.promise;
 
-            if (cache.allCountries) {
-                deferred.resolve(cache.allCountries);
-                dbg.log2('#upgradePlanServices > getAllCountriesList > return cached value');
-                return deferred.promise;
-            }
-
-            dbg.log2('#upgradePlanServices > getAllCountriesList > make rest call');
-            upgradePlanRestApi.getAllCountries.get({}, function(res) {
-                dbg.log2('#upgradePlanServices > getAllCountriesList > rest call responds');
-                deferred.resolve(res);
-                cache.allCountries = res;
-            });
-
-            return deferred.promise;
-
-        }
-
-        function getAllCurrenciesList() {
-            var deferred = $q.defer();
-
-            if (cache.allCurrencies) {
-                deferred.resolve(cache.allCurrencies);
-                dbg.log2('#upgradePlanServices > getAllCurrenciesList > return cached value');
-                return deferred.promise;
-            }
-
-            dbg.log2('#upgradePlanServices > getAllCurrenciesList > make rest call');
-            upgradePlanRestApi.getAllCurrencies.get({}, function(res) {
-                dbg.log2('#upgradePlanServices > getAllCurrenciesList > rest call responds');
-                deferred.resolve(res);
-                cache.allCurrencies = res;
-            });
-
-            return deferred.promise;
-
-        }
-
-        function getPlans() {
-            var deferred = $q.defer();
-
-            if (cache.allCurrencies) {
-                deferred.resolve(cache.getPlans);
-                dbg.log2('#upgradePlanServices > getPlans > return cached value');
-                return deferred.promise;
-            }
-
-            dbg.log2('#upgradePlanServices > getPlans > make rest call');
-            upgradePlanRestApi.getPlans.get({}, function(res) {
-                dbg.log2('#upgradePlanServices > getPlans > rest call responds');
-                deferred.resolve(res);
-                cache.getPlans = res;
-            });
-
-            return deferred.promise;
-
-        }
-
-        /**
-         * Parse and format imput to credit card like string
-         * xxxx - xxxx - xxxx - xxxx ( - xxx )
-         * @param ccInput
-         * @returns {string}
-         */
-        function formatCreditCardNumber(ccInput) {
-            var cardNumberError = false;
-
-            ccInput = ccInput.replace(/\D/g,'');
-
-            // Set maxim amount of digits that allowed for credit cards (19)
-            if (ccInput.length > 19) ccInput = ccInput.substr(0,19);
-
-            // Split to nice view 'xxxx - xxxx - xxxx - xxxx'
-            var arr = ccInput.match(/.{1,4}/g);
-            if (!arr) return;
-            var str = '';
-            for (var i = 0; i < arr.length; i++) {
-                if (i != arr.length-1)	{
-                    str = str + arr[i] + ' - ';
-                } else {
-                    str = str + arr[i];
-                }
-            }
-            ccInput = str;
-
-            return ccInput;
-        }
-
-        function submitUpgrade(planDetails, paymentDetails) {
-            upgradePlanRestApi.upgrade.post({planDetails: planDetails, paymentDetails: paymentDetails})
-        }
     }
+
+    function getAllCurrenciesList() {
+      var deferred = $q.defer();
+
+      if (cache.allCurrencies) {
+        deferred.resolve(cache.allCurrencies);
+        dbg.log2('#upgradePlanServices > getAllCurrenciesList > return cached value');
+        return deferred.promise;
+      }
+
+      dbg.log2('#upgradePlanServices > getAllCurrenciesList > make rest call');
+      upgradePlanRestApi.getAllCurrencies.get({}, function (res) {
+        dbg.log2('#upgradePlanServices > getAllCurrenciesList > rest call responds');
+        deferred.resolve(res);
+        cache.allCurrencies = res;
+      });
+
+      return deferred.promise;
+
+    }
+
+    function getPlans() {
+      var deferred = $q.defer();
+
+      if (cache.allCurrencies) {
+        deferred.resolve(cache.getPlans);
+        dbg.log2('#upgradePlanServices > getPlans > return cached value');
+        return deferred.promise;
+      }
+
+      dbg.log2('#upgradePlanServices > getPlans > make rest call');
+      upgradePlanRestApi.getPlans.get({}, function (res) {
+        dbg.log2('#upgradePlanServices > getPlans > rest call responds');
+        deferred.resolve(res);
+        cache.getPlans = res;
+      });
+
+      return deferred.promise;
+
+    }
+
+    /**
+     * Parse and format imput to credit card like string
+     * xxxx - xxxx - xxxx - xxxx ( - xxx )
+     * @param ccInput
+     * @returns {string}
+     */
+    function formatCreditCardNumber(ccInput) {
+      var cardNumberError = false;
+
+      ccInput = ccInput.replace(/\D/g, '');
+
+      // Set maxim amount of digits that allowed for credit cards (19)
+      if (ccInput.length > 19) ccInput = ccInput.substr(0, 19);
+
+      // Split to nice view 'xxxx - xxxx - xxxx - xxxx'
+      var arr = ccInput.match(/.{1,4}/g);
+      if (!arr) return;
+      var str = '';
+      for (var i = 0; i < arr.length; i++) {
+        if (i != arr.length - 1) {
+          str = str + arr[i] + ' - ';
+        } else {
+          str = str + arr[i];
+        }
+      }
+      ccInput = str;
+
+      return ccInput;
+    }
+
+    function submitUpgrade(planDetails, paymentDetails) {
+      upgradePlanRestApi.upgrade.post({planDetails: planDetails, paymentDetails: paymentDetails})
+    }
+
+    function  getYearsArray() {
+      var currentYear = parseInt(moment().format('YYYY'));
+      var output = [currentYear];
+
+      for (var i = 0, len = 6; i < len ; i++) {
+        output.push(currentYear+1+i);
+      }
+
+      return output;
+    }
+    function getDaysArray() {
+      return [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31];
+    }
+  }
 
 })();
 
