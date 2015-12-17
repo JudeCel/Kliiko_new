833538c4c7b013a1999968b80d8970b3bdb29686
GuskiS <edvards@lazdans.lv>
2015-12-17 11:30:14 +0200

    Fixed API route

--- public/js/ngApp/components/dashboard-accountProfile-accountManagers/AccountManagerServices.js
+++ public/js/ngApp/components/dashboard-accountProfile-accountManagers/AccountManagerServices.js
@@ -1,11 +1,11 @@
 (function () {
   'use strict';
   angular.module('KliikoApp').factory('AccountManagerServices', AccountManagerServices);
-  AccountManagerServices.$inject = ['$q', 'globalSettings', '$resource', 'dbg'];
+  AccountManagerServices.$inject = ['$q', '$resource', 'dbg'];
 
-  function AccountManagerServices($q, globalSettings, $resource, dbg) {
+  function AccountManagerServices($q, $resource, dbg) {
     var accountManagerRestApi = {
-      accountManager: $resource(globalSettings.restUrl + '/accountManager', {}, { post: { method: 'POST' } }),
+      accountManager: $resource('/api/accountManager', {}, { post: { method: 'POST' } }),
     };
 
     var cache = {};
