b0e1119d669ac798487f2069112d9a96ab9f164f
Pavel Karlovich <p.karlovich@gmail.com>
2015-12-30 11:20:26 +0200

    [=] add envs for settings

--- public/js/ngApp/settings/appGlobalSettings.js
+++ public/js/ngApp/settings/appGlobalSettings.js
@@ -18,6 +18,18 @@ function globalSettingsFactory() {
     this.development.appUrl = this.development.protocol+this.development.siteUrl+':'+this.development.port;
     //this.development.restApiFullUrl = this.development.protocol+this.development.siteUrl+':'+this.development.port+this.development.restUrl;
 
+    this.staging.protocol = 'http://';
+    this.staging.siteUrl = coreAppSettings.domain;
+    this.staging.port = coreAppSettings.port;
+    this.staging.restUrl = coreAppSettings.restApiUrl;
+    this.staging.appUrl = this.staging.protocol+this.staging.siteUrl+':'+this.staging.port;
+
+    this.production.protocol = 'http://';
+    this.production.siteUrl = coreAppSettings.domain;
+    this.production.port = coreAppSettings.port;
+    this.production.restUrl = coreAppSettings.restApiUrl;
+    this.production.appUrl = this.production.protocol+this.production.siteUrl+':'+this.production.port;
+
     this[mode].mode = mode;
 
     this[mode].logAndDebugSettings = getLogAndDebugSettings();
