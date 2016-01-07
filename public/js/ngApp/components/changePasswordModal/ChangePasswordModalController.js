aaea83e03d872429c65c142d96b75a72034db224
Lauris Bligzna <bligzna.lauris@gmail.com>
2016-01-07 11:12:59 +0200

    clear fields after submit

--- public/js/ngApp/components/changePasswordModal/ChangePasswordModalController.js
+++ public/js/ngApp/components/changePasswordModal/ChangePasswordModalController.js
@@ -10,17 +10,18 @@
     dbg.log2('#ChangePasswordModalController  started');
     var vm = this;
 
+    vm.passwordData = {};
     vm.changePasswordData = changePasswordData;
     vm.cancel = cancel;
 
     function changePasswordData(data, form) {
       user.changeUserPassword(data, form).then(function (res) {
+        vm.passwordData = {};
         if(res.error){
           messenger.error(res.error);
         }else{
+          cancel();
           messenger.ok(res.message);
-          form.$setPristine();
-          form.$setUntouched();
         }
       });
     }
