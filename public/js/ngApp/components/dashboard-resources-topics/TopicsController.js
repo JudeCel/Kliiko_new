74136b351cdee5938e055af225ad97fe08ca002e
Pavel Karlovich <p.karlovich@gmail.com>
2016-01-14 12:18:51 +0200

    .save

--- public/js/ngApp/components/dashboard-resources-topics/TopicsController.js
+++ public/js/ngApp/components/dashboard-resources-topics/TopicsController.js
@@ -11,7 +11,12 @@
 
     var vm = this;
 
-
+    vm.list = [
+      {name: 'This Is topic name', id: 'topic100500', sessions: [{name:'That is session Name', id: 1},{name:'That is session Name2', id: 2}]},
+      {name: 'This Is topic name2', id: 'topic100500', sessions: [{name:'That is session Name2', id: 2}]},
+      {name: 'This Is topic name3', id: 'topic100500', sessions: [{name:'That is session Name3', id: 3}]},
+      {name: 'This Is topic name4', id: 'topic100500', sessions: [{name:'That is session Name2', id: 2},{name:'That is session Name2', id: 2},{name:'That is session Name4', id: 4}]}
+    ]
   }
 
 
