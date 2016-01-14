10d48d741d596638171af08c873758e9dc1da5a5
Pavel Karlovich <p.karlovich@gmail.com>
2016-01-14 13:33:33 +0200

    [+] topics list

--- public/js/ngApp/components/dashboard-resources-topics/TopicsController.js
+++ public/js/ngApp/components/dashboard-resources-topics/TopicsController.js
@@ -5,19 +5,26 @@
   module('KliikoApp').
   controller('TopicsController', TopicsController);
 
-  TopicsController.$inject = ['dbg'];
-  function TopicsController(dbg) {
+  TopicsController.$inject = ['dbg', 'domServices'];
+  function TopicsController(dbg, domServices) {
     dbg.log2('#TopicsController controller started');
 
     var vm = this;
 
     vm.list = [
-      {name: 'This Is topic name', id: 'topic100500', sessions: [{name:'That is session Name', id: 1},{name:'That is session Name2', id: 2}]},
+      {name: 'This Is topic name', id: 'topic100500', sessions: [{name:'That is session Name', id: 1, status: 'Closed'},{name:'That is session Name2', id: 2, status: 'Opened'}]},
       {name: 'This Is topic name2', id: 'topic100500', sessions: [{name:'That is session Name2', id: 2}]},
       {name: 'This Is topic name3', id: 'topic100500', sessions: [{name:'That is session Name3', id: 3}]},
       {name: 'This Is topic name4', id: 'topic100500', sessions: [{name:'That is session Name2', id: 2},{name:'That is session Name2', id: 2},{name:'That is session Name4', id: 4}]}
-    ]
+    ];
+
+    //vm.list = []
+
+    vm.createNew = createNewTopic;
+
+    function createNewTopic() {
+      domServices.modal('createNewTopic');
+    }
+
   }
-
-
 })();
