bd1a459c5419eb28960fe1244bab9055db48ef71
DainisL <dainis186@gmail.com>
2016-01-21 10:24:29 +0200

    refactoring

--- middleware/fileUploader.js
+++ middleware/fileUploader.js
@@ -21,30 +21,38 @@ const VALIDATIONS = {
 module.exports = function upload(options) {
   options = options || {}
   let storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-      let path = (options.path || config.paths.fsPath + "/" + 'public/uploads/')
-      fs.stat(path, function (err, stats) {
-        if(!stats) {
-          fs.mkdir(path);
-        }
-        cb(null, path);
-      });
-    },
-    fileFilter: function (req, file, cb) {
-      if (_.includes(VALIDATIONS.fileTypes, req.headers['content-type'])) {
-        cb(null, true);
-      }else {
-        cb(new Error(req.headers['content-type'] +' are not allowed'));
-      }
-    },
-    filename: function (req, file, cb) {
-      let re = /(?:\.([^.]+))?$/;
-      let name = file.originalname.split(".")[0]
-      let extension = '.' + re.exec(file.originalname)[1];
-      cb(null, name + (new Date().getTime()) + extension);
-    }
+    destination: destination(options),
+    fileFilter: fileFilter,
+    filename: filename
   });
 
   let upload =  multer({ storage: storage, limits: { fieldSize: VALIDATIONS.maxSize * MEGABYTE } });
-  return upload.single('uploadedfile')
+  return upload.single('uploadedfile');
+}
+
+function fileFilter(req, file, cb) {
+  if (_.includes(VALIDATIONS.fileTypes, req.headers['content-type'])) {
+    cb(null, true);
+  }else {
+    cb(new Error(req.headers['content-type'] +' are not allowed'));
+  }
+}
+
+function filename(req, file, cb) {
+  let re = /(?:\.([^.]+))?$/;
+  let name = file.originalname.split(".")[0]
+  let extension = '.' + re.exec(file.originalname)[1];
+  cb(null, name + (new Date().getTime()) + extension);
+}
+
+function destination(options) {
+  return function(req, file, cb) {
+    let path = (options.path || config.paths.fsPath + '/public/uploads/');
+    fs.stat(path, function (err, stats) {
+      if(!stats) {
+        fs.mkdir(path);
+      }
+      cb(null, path);
+    });
+  }
 }
