92ae6de9260c2bab41cca502b48d988352b24c46
GuskiS <edvards@lazdans.lv>
2016-02-05 15:06:57 +0200

    Added twilio send sms functionality

---
+++ test/lib/twilio.js
@@ -0,0 +1,60 @@
+'use strict';
+
+var twilioRepo = require('./../../lib/twilio');
+var config = require('config');
+var assert = require('chai').assert;
+var _ = require('lodash');
+
+describe('LIB - Twilio', function() {
+  describe('#sendSms', function() {
+    function multipleMobiles(count, mobile) {
+      let array = new Array(count);
+      array.fill(mobile || config.get('twilioSenderNumber'));
+      return array;
+    };
+
+    describe('happy path', function() {
+      function provider(params, callback) {
+        callback();
+      };
+
+      it('should succeed on sending 1 sms', function (done) {
+        twilioRepo.sendSms(multipleMobiles(1), 'Test message', provider).then(function(message) {
+          assert.equal(message, twilioRepo.messages.allSmsSent);
+          done();
+        }, function(error) {
+          done(error);
+        });
+      });
+
+      it('should succeed on sending all sms', function (done) {
+        twilioRepo.sendSms(multipleMobiles(3), 'Test message', provider).then(function(message) {
+          assert.equal(message, twilioRepo.messages.allSmsSent);
+          done();
+        }, function(error) {
+          done(error);
+        });
+      });
+    });
+
+    describe('sad path', function() {
+      function errorProvider(error) {
+        return function(params, callback) {
+          callback({ message: error });
+        };
+      }
+
+      it('should fail because not valid phone number', function (done) {
+        let errorMessage = "The 'To' number nonNumberMobile is not a valid phone number.";
+        let provider = errorProvider(errorMessage);
+
+        twilioRepo.sendSms(multipleMobiles(1, 'nonNumberMobile'), 'Test message', provider).then(function(message) {
+          done('Should not get here!');
+        }, function(error) {
+          assert.equal(error, errorMessage);
+          done();
+        });
+      });
+    });
+  });
+});
