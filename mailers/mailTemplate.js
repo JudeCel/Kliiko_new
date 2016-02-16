ce15cf66ef9fa6816566f00ed664cb225987fb01
Alexander Semionov <alexander.semionov@diatomenterprises.com>
2016-02-16 12:23:46 +0200

    parsing html template to send images via email correctly

--- mailers/mailTemplate.js
+++ mailers/mailTemplate.js
@@ -4,20 +4,47 @@ var config = require('config').get("server");
 var helpers = require('./helpers');
 var mailTemplateService = require('../services/mailTemplate');
 var ical = require('ical-generator');
+var sanitizeHtml = require('sanitize-html');
 
 var mailFrom = helpers.mailFrom();
 var transporter = helpers.createTransport();
 
-function sendMailWithTemplate(template, mailParams, callback) {  
+function extractImageResources(tepmlateHtml) {
+  var resources = [];
+  return {html: sanitizeHtml(tepmlateHtml, {
+      transformTags: {
+        'img': function(tagName, attribs) {
+            let filename = attribs.src.split('/');
+            let name = filename[filename.length - 1];
+            let path = "public" + attribs.src;
+            resources.push({filename: name, path: path, cid: name+"@kliiko"});
+            attribs.src = "cid:"+name+"@kliiko";
+            return {
+                tagName: tagName,
+                attribs: attribs
+            };
+        }
+      },
+      allowedTags:false,
+      allowedAttributes: false,
+      allowedSchemes: ['data', 'cid', 'http', 'https']
+    }),
+    resources: resources
+  }
+}
+
+function sendMailWithTemplate(template, mailParams, callback) {
+  let parsedTemplate = extractImageResources(template.content);
   transporter.sendMail({
     from: mailFrom,
     to: mailParams.email,
     subject: template.subject,
-    html: template.content
+    html: parsedTemplate.html,//template.content
+    attachments: parsedTemplate.resources
   }, callback);
 }
 
-function sendMailWithTemplateAndCalendarEvent(template, mailParams, callback) {  
+function sendMailWithTemplateAndCalendarEvent(template, mailParams, callback) {
     let cal = ical({domain: config.domain, name: template.name});
     let event = cal.createEvent({
         start: mailParams.start,
@@ -26,16 +53,16 @@ function sendMailWithTemplateAndCalendarEvent(template, mailParams, callback) {
         summary: template.name,
         organizer: mailFrom
     });
-    
+
     /*
     // add attendees to event when known
         event.attendees([
-        {email: 'warlockxins@gmail.com', name: 'Person A'},        
+        {email: 'warlockxins@gmail.com', name: 'Person A'},
     ]);
     */
-    
+
     let calendarData =  cal.toString();
-    
+
     transporter.sendMail({
       from: mailFrom,
       to: mailParams.email,
