249c4ab1e63dee63c7034c586cad2936b342b1ec
Lauris Bligzna <bligzna.lauris@gmail.com>
2016-03-18 07:54:59 +0200

    saving progress

--- public/js/ngApp/appRouter.js
+++ public/js/ngApp/appRouter.js
@@ -287,6 +287,41 @@
         }]
 
       })
+
+      .state('dashboard.smsCredits', {
+        url: "/smsCredits",
+        views: {
+          'resourcesContent': {templateUrl: prePath + "dashboard-smsCredits/dashboard-content.htm"}
+        },
+
+        resolve: {
+          checkPermission: ['$q', '$timeout', 'dbg', 'ngProgressFactory', 'banners',function($q, $timeout, dbg, ngProgressFactory, banners) {
+            var deferred = $q.defer();
+
+            user.canAccess('bannerMessages').then(
+              function(res) {
+                var progressbar = ngProgressFactory.createInstance();
+                progressbar.start();
+
+                // load rarely needed module: ng-file-upload
+                banners.initUpload().then(function() { progressbar.complete(); deferred.resolve(); });
+
+              },
+
+              function(err) { dbg.warn(err); }
+
+            );
+            return deferred.promise;
+          }]
+        },
+
+        onEnter: ['dbg', function (dbg) {
+          dbg.rs('dashboard.resources.gallery is on');
+        }]
+
+      })
+
+
       .state('dashboard.resources.emailTemplates', {
         url: "/email-templates",
         views: {
