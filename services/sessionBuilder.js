59ae5e361fe0428d4d2931ea9e058722df18e43b
Lauris Bligzna <bligzna.lauris@gmail.com>
2016-04-11 15:19:53 +0300

    fixed steo 3 validation for necesary email templates.

--- services/sessionBuilder.js
+++ services/sessionBuilder.js
@@ -39,7 +39,7 @@ const MESSAGES = {
       topics: 'No topics selected'
     },
     thirdStep: {
-      emailTemplates: "Not enough mail templates provided, needed "
+      emailTemplates: "You need to copy each of the required e-mail template."
     },
     fourthStep: {
       participants: 'No participants invited'
@@ -742,19 +742,34 @@ function validateStepTwo(params) {
 
 function validateStepThree(params) {
   let deferred = q.defer();
+  let mailCategories = ["firstInvitation", "confirmation", "notThisTime", "notAtAll", "closeSession"];
+  let baseTemplateQuery = {category:{ $in: mailCategories }};
+  let include = [{ model: models.MailTemplateBase, attributes: ['id', 'name', 'systemMessage', 'category'], where: baseTemplateQuery }];
 
   findSession(params.id, params.accountId).then(function(session) {
-    models.MailTemplate.count({
+    models.MailTemplate.findAll({
       where: {
         sessionId: session.id,
         isCopy: true,
         required: true
-      }
-    }).then(function(count) {
+      },
+      include: include
+    }).then(function(templates) {
+      let uniqueCopies = [];
       let errors = {};
 
-      if(count < MIN_MAIL_TEMPLATES){
-        errors.emailTemplates = MESSAGES.errors.thirdStep.emailTemplates + MIN_MAIL_TEMPLATES;
+      _.forEach(mailCategories, function(category) {
+        _.forEach(templates, function(template) {
+          if(template.MailTemplateBase.category == category){
+            uniqueCopies.push(template.MailTemplateBase.category);
+          }
+        });
+      })
+
+      uniqueCopies = _.uniq(uniqueCopies);
+
+      if(uniqueCopies.length < MIN_MAIL_TEMPLATES){
+        errors.emailTemplates = MESSAGES.errors.thirdStep.emailTemplates;
       }
 
       _.isEmpty(errors) ? deferred.resolve() : deferred.reject(errors);
