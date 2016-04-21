1f019f7a84fa239da0e100b01fd53537fc5cd3b9
Janis Miezitis <janjiss@gmail.com>
2016-04-21 16:44:19 +0300

    Added main css to gitifnore

--- gulpfile.js
+++ gulpfile.js
@@ -1,229 +1,74 @@
 // gulp task manager
+require('dotenv-extended').load({
+    errorOnMissing: true
+});
+
 var gulp = require('gulp'),
-  jscs = require('gulp-jscs'),
-  util = require('gulp-util'),
-  gulpPrint = require('gulp-print'),
-  gulpif = require('gulp-if'),
-  args = require('yargs').argv,
-  config = require('./gulp.config')(),
-  sass = require('gulp-sass'),
-  autoprefixer = require('gulp-autoprefixer'),
-  del = require('del'),
-  inject = require('gulp-inject'),
-  nodemon = require('gulp-nodemon'),
-  plumber = require('gulp-plumber'),
-  browserSync = require('browser-sync'),
+  http = require('http'),
   taskListing = require('gulp-task-listing'),
-  newer = require('gulp-newer'),
-  templateCache = require('gulp-angular-templatecache'),
-  imagemin = require('gulp-imagemin'),
   concat = require('gulp-concat'),
   uglify = require('gulp-uglify'),
   sourcemaps = require('gulp-sourcemaps'),
   rename = require('gulp-rename'),
   order = require('gulp-order'),
   exit = require('gulp-exit'),
-  useref = require('gulp-useref'),
-  minifyHtml = require('gulp-minify-html'),
-  nodeInspector = require('gulp-node-inspector'),
   clean_css = require('gulp-clean-css'),
-  args = require('yargs').argv,
-  port = process.env.PORT || config.defaultPort;
+  plumber = require('gulp-plumber'),
+  app = require("./app.js"),
+  nodemon = require('gulp-nodemon'),
+  watch = require('gulp-watch'),
+  sass = require('gulp-sass'),
+  batch = require('gulp-batch'),
+  autoprefixer = require('gulp-autoprefixer'),
+  config = require('./gulp.config.js')();
 
-gulp.task('help', taskListing);
-gulp.task('default', ['help']);
+gulp.task('default', ['serve-dev', 'watch']);
 
-gulp.task('templatecache', function (){
-  log('Creating AngularJS $templateCache');
-
-  return gulp
-    .src(config.htmlTemplates)
-    .pipe(minifyHtml({empty: true}))
-    .pipe(templateCache(config.templateCache.file,
-      config.templateCache.options))
-    .pipe(gulp.dest(config.temp));
-
+gulp.task('serve-dev', ['build-js', 'build-css'], function () {
+  var port = process.env.SERVER_PORT;
+  var environment = process.env.NODE_ENV;
+  app.set('port', port);
+  var server = http.createServer(app);
+  server.listen(port);
 });
 
-gulp.task('optimize', ['inject'], function(){
-  log('Optimizing the javascript, css, html');
-  var templateCache = config.temp + config.templateCache.file;
-  var assets = useref.assets({searchPath: './'});
-  return gulp
-    .src(config.index)
-    .pipe(plumber())
-    .pipe(inject(gulp.src(templateCache,{read: false}), {starttag: '<!--inject:templates:js-->'}))
-    .pipe(assets)
-    .pipe(assets.restore())
-    .pipe(useref())
-    .pipe(gulp.dest(config.build));
-} );
-
-gulp.task('clean-code', function(done){
-  var files = [].concat(
-    config.temp + '**/*.js',
-    config.build + '**/*.html,',
-    config.build + 'js/**/*.js'
-
-  );
-  clean(files,done);
-});
-
-gulp.task('bootstrap', function () {
-  log('Compiling Bootstrap SASS --> CSS');
-  return gulp
-    .src(config.bootstrap)
-    .pipe(plumber())
-
-    .pipe(sass())
-    //   .on('error', errorLoger)
-    .pipe(autoprefixer({browsers: ['last 2 version', '> 5%']}))
-    .pipe(gulp.dest(config.css))
-});
-gulp.task('sass', function () {
-  log('Compiling SASS --> CSS');
+gulp.task('build-sass', function () {
   return gulp
     .src(config.sass)
     .pipe(plumber())
     .pipe(sass())
-    //   .on('error', errorLoger)
     .pipe(autoprefixer({browsers: ['last 2 version', '> 5%']}))
     .pipe(gulp.dest(config.css));
 });
 
-  gulp.watch(config.clientApp, ['sass']);
+gulp.task('watch', ['watch-css', 'watch-js']);
 
-gulp.task('images', function(){
-  log('Copying and compressing the images');
-  return gulp
-    .src(config.images)
-    .pipe(newer(config.build + 'images'))
-    .pipe(imagemin({optimization: 4}))
-    .pipe(gulp.dest(config.build + 'images'))
-});
-gulp.task('fonts', function(){
-  return gulp
-    .src(config.fonts)
-    .pipe(gulp.dest(config.build + 'fonts'));
+gulp.task('watch-css', function () {
+  watch(['**/*.scss', '**/*.css', '!public/css/compiled/**/*.css', '!public/css/main.css'], batch(function (events, done) {
+    gulp.start('build-css', done);
+  }));
 });
 
-gulp.task('inject', ['bootstrap', 'sass', 'templatecache'], function () {
-  log('Wire up the app css into html and call wiredep');
-
-  return gulp
-    .src(config.index)
-    .pipe(inject(gulp.src(config.css)))
-    .pipe(gulp.dest(config.layout))
-
+gulp.task('watch-js', function () {
+  watch(['**/*.js', "!public/js/compiled/**/*.js"], batch(function (events, done) {
+    gulp.start('build-js', done);
+  }));
 });
 
-gulp.task('inspect', function () {
-  var debugPort = 5858;
-  var webPort = 8085;
+gulp.task('help', taskListing);
+var server = require('gulp-server-livereload');
 
-    gulp.src([]).pipe(nodeInspector({
-        debugPort: debugPort,
-        webHost: '0.0.0.0',
-        webPort: webPort,
-        preload: false
+gulp.task('webserver', function() {
+  gulp.src('app')
+    .pipe(server({
+      livereload: true,
+      directoryListing: true,
+      open: true
     }));
 });
 
-gulp.task('serve-dev', (args.debug) ? ['inject', 'inspect'] : ['inject'], function () {
-  var isDev = true;
-  var nodeOptions = {
-    nodeArgs: [],
-    script: config.nodeServer,
-    delayTime: 0,
-    env: {
-      'PORT': port,
-      'NOE_ENV': isDev ? 'dev' : 'build'
-    },
-    watch: [config.server]
-  };
 
-  // Debug using node inspect
-  if (args.debug) {
-        nodeOptions.nodeArgs.push('--debug');
-    }
-
-  return nodemon(nodeOptions)
-    .on('restart', ['vet'], function (ev) {
-      log("*** nodemon restarted");
-      log('*** files changed on restart:\n' + ev);
-      setTimeout(function() {
-        browserSync.notify('reloading now ...');
-        browserSync.reload({stream: false});
-      }, config.browserReloadDelay)
-    })
-
-    .on('start', function () {
-      log("*** nodemon started");
-      startBrowserSync();
-    })
-    .on('crash', function () {
-      log("*** nodemon crashed");
-    })
-    .on('exit', function () {
-      log("*** nodemon exited cleanly");
-    });
-
-});
-
-function startBrowserSync() {
-  if (browserSync.active) {
-    return;
-  }
-
-  function changeEvent(event) {
-    var srcPattern = new RegExp('/.*(?=/' + config.source + ')/');
-    log('File' + event.path.replace(srcPattern, '') + ' ' + event.type);
-  }
-
-  log('Starting browser-sync on port ' + port);
-
-  gulp.watch([config.sass], ['bootstrap'])
-    .on('change', function (event) {
-      changeEvent(event);
-    });
-
-  var options = {
-    proxy: config.dns + port,
-    open: false,
-    port: 3000,
-    files: [
-      config.migrations,
-      config.services,
-      config.middleware,
-      config.htmlTemplates,
-      config.jsFiles,
-      config.config,
-      config.clientApp,
-      config.repositories,
-      config.models,
-      config.routes,
-      config.views,
-      '!' + config.sass
-    ],
-    ghostNode: {
-      clicks: true,
-      location: false,
-      forms: true,
-      scroll: true
-    },
-    injectChanges: true,
-    logFileChanges: true,
-    loglevel: 'debug',
-    logPrefix: 'gulp-pattern',
-    notify: true,
-    reloadDelay: 0
-  };
-  browserSync(options);
-}
-gulp.task('clean', function (done) {
-  var files = config.temp + '**/*.css';
-  clean(files, done)
-});
+// Asset serving
 
 var frontendScripts = [
   "public/js/vendors/jQuery/jquery-2.1.4.min.js",
@@ -276,7 +121,7 @@ var frontendStyles = [
   "public/js/vendors/angular-material/1.0.1/angular-material.min.css"
 ]
 
-gulp.task('build-css', function(){
+gulp.task('build-css', ['build-sass'], function(){
   return gulp.src(frontendStyles)
   .pipe(sourcemaps.init())
   .pipe(clean_css())
@@ -316,24 +161,5 @@ gulp.task('minify-css', function() {
     .pipe(gulp.dest('public/js/compiled'));
 });
 
-gulp.task('default', ['build-js', 'build-css', 'serve-dev']);
 
 gulp.task('build-prod', ['build-css-prod', 'build-js-prod']);
-
-function clean(path, done) {
-  log('Cleanening:' + util.colors.blue(path));
-  del(path, done);
-}
-
-function log(msg) {
-  if (typeof (msg) === 'object') {
-    for (var item in msg) {
-      if (msg.hasOwnProperty(item)) {
-        util.log(util.colors.blue(msg[item]));
-      }
-    }
-  }
-  else {
-    util.log(util.colors.blue(msg));
-  }
-}
