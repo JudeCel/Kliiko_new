e9ff1b8613d698f93d2c4650e0ea4bbf9c776d37
Lauris Bligzna <bligzna.lauris@gmail.com>
2016-04-21 11:37:51 +0300

    DE230 Contact details can not be updated

--- public/js/ngApp/components/contactDetailsModal/ContactDetailsModalController.js
+++ public/js/ngApp/components/contactDetailsModal/ContactDetailsModalController.js
@@ -75,9 +75,7 @@
 
 
     function updateUserData(data, form) {
-      console.log(validatePhoneNumber())
-
-      if(validatePhoneNumber() && validateLandlineNumber(data.landlineNumber)){
+      if(validatePhoneNumber(data.mobile) && validateLandlineNumber(data.landlineNumber)){
         getPhoneCountryData();
         getLandlineNumberCountryData();
 
@@ -109,8 +107,8 @@
       }
     }
 
-    function validatePhoneNumber() {
-      if(!validatePhone()){
+    function validatePhoneNumber(mobile) {
+      if(mobile === undefined && !validatePhone()){
         messenger.error("The mobile number for this country is not valid.");
         return false;
       }
@@ -118,7 +116,7 @@
     }
 
     function validateLandlineNumber(landlineNumber) {
-      if(landlineNumber && !validLandlineNumber()){
+      if(landlineNumber === undefined && !validLandlineNumber()){
         messenger.error("The landline number for this country is not valid.");
         return false;
       }
