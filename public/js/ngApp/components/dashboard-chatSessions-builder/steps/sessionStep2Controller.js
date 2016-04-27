8bbfcc5b18eab6cba13ce225c1fed49cb4843d7c
BLauris <bligzna.lauris@gmail.com>
2016-04-27 23:27:38 +0300

    Saving progress...
    Added angulas sortable-ui directive, so I can start to rewrite the wrong
    topic drag and drop logic. Need to discuss with Janis how it should
    actually work.

--- public/js/ngApp/components/dashboard-chatSessions-builder/steps/sessionStep2Controller.js
+++ public/js/ngApp/components/dashboard-chatSessions-builder/steps/sessionStep2Controller.js
@@ -28,6 +28,29 @@
     vm.selectAllTopics = selectAllTopics;
     vm.chatSessionTopicsList = [];
 
+    /////////////////////////////////////////
+
+    $scope.list = vm.selectAllTopics;
+    $scope.sortingLog = [];
+
+    $scope.sortableOptions = {
+      update: function(e, ui) {
+        var logEntry = tmpList.map(function(i){
+          return i.value;
+        }).join(', ');
+        $scope.sortingLog.push('Update: ' + logEntry);
+      },
+      stop: function(e, ui) {
+        // this callback has the changed model
+        var logEntry = tmpList.map(function(i){
+          return i.value;
+        }).join(', ');
+        $scope.sortingLog.push('Stop: ' + logEntry);
+      }
+    };
+
+    /////////////////////////////////////////
+
     vm.initController = function() {
       initStep();
       vm.session = builderServices.session;
