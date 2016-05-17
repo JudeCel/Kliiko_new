e3b3bfb1510ca47ad4e8e47ba33f0b0b6be87e45
DainisL <dainis186@gmail.com>
2016-05-17 13:15:07 +0300

    change relation from topic to session topic

--- models/message.js
+++ models/message.js
@@ -4,17 +4,24 @@ module.exports = (Sequelize, DataTypes) => {
   var Message = Sequelize.define('Message', {
     id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
     sessionMemberId: {type: DataTypes.INTEGER, allowNull: false },
-    topicId: { type: DataTypes.INTEGER, allowNull: false },
+    sessionTopicId: { type: DataTypes.INTEGER, allowNull: false },
     replyId: { type: DataTypes.INTEGER, allowNull: true },
     emotion: { type: DataTypes.INTEGER, allowNull: false, defaultValue: 1 },
     body: { type: DataTypes.STRING, allowNull: false },
     star: { type: DataTypes.BOOLEAN, allowNull: false, defaultValue: false }
   }, {
     timestamps: true,
+    indexes: [
+      { fields: ['sessionMemberId'] },
+      { fields: ['sessionTopicId'] },
+      { fields: ['replyId'] },
+      { fields: ['star'] },
+    ],
     classMethods: {
       associate: function(models) {
         Message.hasMany(models.Vote, { foreignKey: 'messageId' });
         Message.belongsTo(models.SessionMember, { foreignKey: 'sessionMemberId' });
+        Message.belongsTo(models.SessionTopics, { foreignKey: 'sessionTopicId' });
       }
     }
   });
