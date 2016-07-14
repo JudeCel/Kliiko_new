43dbcce55d66bff1c471a982dbae355534f1c495
GuskiS <edvards@lazdans.lv>
2016-07-14 16:34:15 +0300

    TA925: UI: Remove step 3, move sign

--- public/js/ngApp/components/dashboard-chatSessions-builder/steps/sessionStep3Controller.js
+++ public/js/ngApp/components/dashboard-chatSessions-builder/steps/sessionStep3Controller.js
@@ -6,35 +6,6 @@
   SessionStep3Controller.$inject = ['dbg', 'sessionBuilderControllerServices', 'messenger', '$state',  '$filter', '$scope', 'SessionModel'];
   function SessionStep3Controller(dbg, builderServices, messenger, $state, $filter, $scope, SessionModel) {
     dbg.log2('#SessionBuilderController 3 started');
-    var vm = this;
-    vm.mailTemplateList = [];
-    vm.session = builderServices.session;
-    vm.showTemplateStatus = true;
-
-    vm.getPreparedMailTemplateList = function() {
-      vm.session.getSessionMailTemplateStatus().then(function(res) {
-        vm.mailTemplateList = res.templates;
-      }, function (err) {
-        messenger.error(err);
-      });
-    }
-
-    vm.editTemplates = function(hideEditor) {
-      vm.showTemplateStatus = !hideEditor;
-      if (vm.showTemplateStatus) {
-        vm.getPreparedMailTemplateList();
-      }
-    }
-
-    vm.isTemplateRequired = function(template) {
-      if (template.name == "Generic") {
-        return "(optional)";
-      } else {
-        return "*";
-      }
-    }
-
-    vm.getPreparedMailTemplateList();
 
   }
 
