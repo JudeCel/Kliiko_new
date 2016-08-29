2131e7619731bf963a9add35501b94c25567b8c2
DainisL <dainis186@gmail.com>
2016-08-29 17:06:56 +0300

    fixed mailer tests

--- test/mailers/invite.js
+++ test/mailers/invite.js
@@ -3,11 +3,11 @@
 var assert = require('chai').assert;
 var inviteMailers = require('./../../mailers/invite.js');
 
-describe('MAILERS - Invite ', () => {
+describe.only('MAILERS - Invite ', () => {
   describe('sendInviteAccountManager ', () => {
     let email = 'testeEmail@gmail.com';
     let token = '56b429319232ba613b76749988de4555';
-    let params = { email: email, token: token };
+    let params = { email: email, token: token, role: "accountManager" };
 
     it('content', (done) =>  {
       inviteMailers.sendInviteAccountManager(params, function(error, result) {
