b72aff935b09444a5b25121a8ef34408644f3864
DainisL <dainis186@gmail.com>
2016-09-12 11:42:19 +0300

    fixed DE710
    added validation for landline Number and allow empty phone

--- services/contactListImport.js
+++ services/contactListImport.js
@@ -238,8 +238,8 @@ function skipInvalidRow(data, contactList) {
 }
 
 function checkKeyValues(rowData, emails, key, row, uniqueRowListCounter, error) {
-  if (key == 'mobile') {
-    if (!constants.phoneRegExp.test(rowData)) {
+  if (_.includes([ 'mobile', 'landlineNumber'], key) ) {
+    if (!_.isEmpty(rowData) && !constants.phoneRegExp.test(rowData)) {
       error[key] = MessagesUtil.contactListImport.error.invalidFormat;
     }
   }
