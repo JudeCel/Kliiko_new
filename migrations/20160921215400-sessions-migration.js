0a8b9c3fa81e5425c5d3b658577f2c18f88e8e7d
Nazar Perepichka <nazar.perepichka@diatomenterprises.com>
2016-10-12 10:18:10 +0300

    sessions migration improve

--- migrations/20160921215400-sessions-migration.js
+++ migrations/20160921215400-sessions-migration.js
@@ -2,16 +2,11 @@
  let Bluebird = require('bluebird');
  let validateError = require('./helpers/errorFilter.js').validateError
 
- let Sequelize = require('sequelize');
- let env       = process.env.NODE_ENV || 'development';
- let config    = require(__dirname + '/../config/config.js')[env];
- let sequelize = new Sequelize(config.database, config.username, config.password, config);
-
  module.exports = {
    up: function (queryInterface, Sequelize) {
      return new Bluebird(function (resolve, reject) {
        queryInterface.addColumn('Sessions', 'type', { type: Sequelize.ENUM, values: ['focus', 'forum'] }).then(function(result) {
-         sequelize.query('UPDATE public."Sessions" SET type = \'focus\'').then(function() {
+         queryInterface.sequelize.query('UPDATE public."Sessions" SET type = \'focus\'').then(function() {
            resolve();
          },function(error) {
            validateError(error, resolve, reject);
