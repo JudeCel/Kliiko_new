049e496e69f7f8aabddf5fb3c0e27e71ba4a035b
Alexander Semionov <alexander.semionov@diatomenterprises.com>
2016-11-03 14:59:17 +0200

    TA1486 change message type

---
+++ migrations/20162010082801-change-messages-body-type-migration.js
@@ -0,0 +1,24 @@
+'use strict';
+ let Bluebird = require('bluebird');
+ let validateError = require('./helpers/errorFilter.js').validateError
+
+ module.exports = {
+   up: function (queryInterface, Sequelize) {
+     return new Bluebird(function (resolve, reject) {
+       queryInterface.changeColumn('Messages', 'body',  { type: Sequelize.TEXT, allowNull: false }).then(function() {
+         resolve();
+       },function(error) {
+         validateError(error, resolve, reject);
+       });
+   });
+   },
+   down: function (queryInterface, Sequelize) {
+     return new Bluebird(function (resolve, reject) {
+       queryInterface.changeColumn('Messages', 'body',  { type: Sequelize.STRING, allowNull: false }).then(function() {
+         resolve();
+       },function(error) {
+         validateError(error, resolve, reject);
+       });
+     });
+   }
+ };
