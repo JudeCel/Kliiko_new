8deab965df13e21421dc573b8d977ae01aa74c35
DainisL <dainis186@gmail.com>
2016-11-16 14:15:47 +0200

    experment with cluster

---
+++ bin/server.js
@@ -0,0 +1,33 @@
+"use strict";
+const cluster = require('cluster');
+/**
+ * Module dependencies.
+ */
+ require('dotenv-extended').load({
+     errorOnMissing: true
+ });
+
+
+  if (cluster.isMaster) {
+  cluster.fork({ROLE: "webServer"});
+  cluster.fork({ROLE: "backgroundWorkerServer"});
+
+  cluster.on('exit', (worker, code, signal) => {
+    console.log(worker);
+    console.log(`worker ${worker.process.pid} died`);
+  });
+
+  } else {
+    switch (process.env.ROLE) {
+      case "webServer":
+        console.log("start web server");
+        require("./www");
+        break;
+      case "backgroundWorkerServer":
+        console.log("background Worker Server");
+        require("./backgroundSideServer");
+        break;
+      default:
+        console.log("Unhandled process role: " + process.env.ROLE);
+    }
+  }
