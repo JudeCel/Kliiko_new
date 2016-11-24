3488b14994e92f29ae43fd51adb7550dd10f1809
DainisL <dainis186@gmail.com>
2016-11-24 16:29:17 +0200

    added dataNormalization task for account users

---
+++ tasks/dataNormalization/accountUsers.js
@@ -0,0 +1,26 @@
+'use strict';
+var {User, AccountUser } = require('./../../models');
+let Bluebird = require('bluebird');
+var _ = require('lodash');
+
+function updateAccountUsersUserId() {
+  return new Bluebird(function (resolve, reject) {
+    User.findAll({attributes: ['id', 'email']}).then((result) => {
+      Bluebird.each(result, (item) => {
+        return new Bluebird(function (resolve, reject) {
+          AccountUser.update({"UserId": item.id}, {where: {"UserId": null, email: item.email }}).then(() =>{
+            resolve();
+          });
+        });
+      }).then(function(result) {
+        resolve();
+      },function(error) {
+        reject(error);
+      })
+    });
+  })
+}
+
+module.exports = {
+  updateAccountUsersUserId: updateAccountUsersUserId,
+}
