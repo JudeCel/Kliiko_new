a9cd7e137874ca70db5610fa546c168291afcc84
DainisL <dainis186@gmail.com>
2016-12-06 20:05:18 +0200

    fixed resque config

--- config/backgroudServer.js
+++ config/backgroudServer.js
@@ -3,29 +3,11 @@ require('dotenv-extended').load({
 });
 
 var config = {
-  "development": {
-    host:      process.env.REDIS_HOST,
-    password:  null,
-    port:      parseInt(process.env.REDIS_PORT),
-    database:  parseInt(process.env.REDIS_DB),
-    namespace: 'dashboard_jobs_dev',
-    looping: true,
-  },
-  "test": {
-    host:      process.env.REDIS_HOST_TEST,
-    password:  null,
-    port:      parseInt(process.env.REDIS_PORT_TEST),
-    database:  parseInt(process.env.REDIS_DB_TEST),
-    namespace: 'dashboard_jobs_test',
-    looping: true,
-  },
-  "production": {
-    host:      process.env.REDIS_HOST,
-    password:  null,
-    port:      parseInt(process.env.REDIS_PORT),
-    database:  parseInt(process.env.REDIS_DB),
-    namespace: 'dashboard_jobs_prod',
-    looping: false,
-  }
+  host:      process.env.REDIS_HOST,
+  password:  null,
+  port:      parseInt(process.env.REDIS_PORT),
+  database:  parseInt(process.env.REDIS_DB),
+  namespace: 'resque',
+  looping: true
 }
 module.exports = config;
