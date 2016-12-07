aa7c214c6f48dd8a724c17a67e3250097feebeec
Nazar Perepichka <nazar.perepichka@diatomenterprises.com>
2016-12-07 11:22:05 +0200

    fix

--- public/js/ngApp/components/dashboard-chatSessions-builder/SessionBuilderController.js
+++ public/js/ngApp/components/dashboard-chatSessions-builder/SessionBuilderController.js
@@ -352,8 +352,7 @@
     }
 
     function canCommentAndRate() {
-      //return isSelectParticipantStep() && isSessionClosed();
-      return true;
+      return isSelectParticipantStep() && isSessionClosed();
     }
   }
 
