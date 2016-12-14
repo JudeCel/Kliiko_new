1cdc77d6b18863d45d1bfb96e8e706e4379980a8
DainisL <dainis186@gmail.com>
2016-12-14 12:08:36 +0200

    update PM2 config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,32 +2,16 @@ module.exports = {
   apps : [{
     "name": "web",
     "script": "./apps/webServer.js",
-    "watch": true,
     "log_file": "./logs/webServer/webServer.log",
     "out_file": "./logs/webServer/out.log",
     "error_file": "./logs/webServer/err.log",
-    env: {
-      "NODE_ENV": "development",
-      instances: 1
-    },
-    env_production : {
-      "NODE_ENV": "production",
-      instances: 2
-    }
+    "exec_mode" : "cluster"
   },{
     "name": "backgroundSideServer",
     "script": "./apps/backgroundSideServer.js",
-    "watch": true,
     "log_file": "./logs/backgroundSideServer/backgroundSideServer.log",
     "out_file": "./logs/backgroundSideServer/out.log",
     "error_file": "./logs/backgroundSideServer/err.log",
-    env: {
-      "NODE_ENV": "development",
-      "instances": 1
-    },
-    env_production : {
-      "NODE_ENV": "production",
-      "instances": 2
-    }
+    "exec_mode" : "cluster"
   }]
 }
