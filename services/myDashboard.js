f3441704fc896d4808de6f490df336504688b46a
DainisL <dainis186@gmail.com>
2017-01-19 11:37:17 +0200

    init TA1522
    Remove old migrations

--- services/myDashboard.js
+++ services/myDashboard.js
@@ -28,6 +28,8 @@ function getAllData(userId, protocol, provider) {
     where: { UserId: userId },
     include: [{
       model: SessionMember,
+      required: false,
+      where: {typeOfCreation: {$ne: 'system'}, role: {$ne: 'observer'}},
       include: [{
         model: Session,
         include: [{
