79a64fb8cf84904f90368d3f1a63da36083e5cb8
papusa <andrey.papusa@diatomenterprises.com>
2017-02-07 13:30:25 +0000

    Mail template index migration file updated.

--- migrations/2017020622700-add-unique-index-to-mail-templates-migration.js
+++ migrations/2017020622700-add-unique-index-to-mail-templates-migration.js
@@ -3,25 +3,24 @@
  let validateError = require('./helpers/errorFilter.js').validateError
 
  module.exports = {
-   up: (queryInterface, Sequelize) => {
-     return new Bluebird((resolve, reject) => {
-       queryInterface.addIndex(
-           'MailTemplates',
-           {
-               name: 'compositeAccountIdMailTemplateBaseIdAndTemplateName',
-               unique: true,
-               fields: ['name', 'AccountId', 'MailTemplateBaseId']
-            }
-         ).then(() => {
+  up: (queryInterface, Sequelize) => {
+    return new Bluebird((resolve, reject) => {
+      queryInterface.addIndex(
+        'MailTemplates',
+        {
+          name: 'compositeAccountIdMailTemplateBaseIdAndTemplateName',
+          unique: true,
+          fields: ['name', 'AccountId', 'MailTemplateBaseId']
+        }).then(() => {
          resolve();
        }, (error) => {
          validateError(error, resolve, reject);
        });
-   });
+    });
    },
    down: (queryInterface, Sequelize) => {
     return queryInterface.removeIndex(
-        'SessionMembers', 
+        'MailTemplates', 
         {
             name: 'compositeAccountIdMailTemplateBaseIdAndTemplateName',
             unique: true,
