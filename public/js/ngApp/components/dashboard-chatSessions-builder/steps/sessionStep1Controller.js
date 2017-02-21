480315a7e97522ac51a0a9f6ca9ae510c47c0768
GuskiS <edvards@lazdans.lv>
2017-02-21 12:48:55 +0200

    Fixed DE1356: Chosen host not displayed in list if he has spectator or guest role

--- public/js/ngApp/components/dashboard-chatSessions-builder/steps/sessionStep1Controller.js
+++ public/js/ngApp/components/dashboard-chatSessions-builder/steps/sessionStep1Controller.js
@@ -175,9 +175,9 @@
         return vm.q;
       }
       else {
-        var facilitator = vm.session.sessionData.facilitator;
+        var facilitator = vm.session.steps.step1.facilitator;
         return function(item) {
-          return ['facilitator', 'accountManager'].indexOf(item.role) > -1 || facilitator && facilitator.AccountUser.email === item.email;
+          return ['facilitator', 'accountManager'].indexOf(item.role) > -1 || facilitator && facilitator.email === item.email;
         };
       }
     }
