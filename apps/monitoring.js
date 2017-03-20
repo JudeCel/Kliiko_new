93fd844c7a8e543ce9bf6a17293af940656afcaa
DainisL <dainis186@gmail.com>
2017-03-20 18:27:51 +0200

    use single monitoring push

--- apps/monitoring.js
+++ apps/monitoring.js
@@ -28,8 +28,7 @@ pm2.connect((err, resp) => {
 
       si.currentLoad().then((load) => {
         si.mem().then((mem) => {
-          infoChannel.push("system_info", { load, mem});
-          infoChannel.push("system_metrics", { metrics });
+          infoChannel.push("system_info_metrics", { load, mem, metrics});
         });
       });
     });
