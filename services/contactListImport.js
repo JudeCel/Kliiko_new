74541ad3065d4a9dc6072c7199c75026e5732958
Nazar Perepichka <nazar.perepichka@diatomenterprises.com>
2017-03-31 15:35:22 +0300

    DE1566

--- services/contactListImport.js
+++ services/contactListImport.js
@@ -149,15 +149,7 @@ function parseXls(emails, deferred, contactList, filePath) {
       ++ rowNr
       data.landlineNumber = data.landlineNumber.toString();
       data.mobile = data.mobile.toString();
-
-      if(data.mobile.length > 0 && !data.mobile.includes("+61")) {
-        data.mobile = "+61 " + data.mobile;
-      }
-
-      if(data.landlineNumber.length > 0 && !data.landlineNumber.includes("+61")) {
-        data.landlineNumber = "+61 " + data.landlineNumber;
-      }
-
+      
       validateRow(emails, contactList, data, uniqRowListCounter).then(function() {
         object.valid.push(data);
         cb();
@@ -204,17 +196,10 @@ function parseCsv(emails, deferred, contactList, filePath) {
 
       if (data.landlineNumber) {
         data.landlineNumber = data.landlineNumber.toString();
-        if(data.landlineNumber.length > 0 && !data.landlineNumber.includes("+61")) {
-          data.landlineNumber = "+61 " + data.landlineNumber;
-        }
       }
 
       if (data.mobile) {
         data.mobile = data.mobile.toString();
-
-        if(data.mobile.length > 0 && !data.mobile.includes("+61")) {
-          data.mobile = "+61 " + data.mobile;
-        }
       }
 
       validateRow(emails, contactList, data, uniqRowListCounter).then(function() {
