eaff1d7edf0d0f496b3c6ecaf387607c428e4f4f
DainisL <dainis186@gmail.com>
2017-05-16 11:54:24 +0300

    DE1587

--- models/validations/length.js
+++ models/validations/length.js
@@ -1,19 +1,20 @@
 'use strict';
 
 var _ = require('lodash');
+var sanitizeHtml = require('sanitize-html');
 var MessagesUtil = require('./../../util/messages');
 
 function length(fieldName, values) {
   return function(value, next) {
     if(value && typeof value == 'string') {
       let min = values.min;
-      if(min && value.length < min) {
+      if(min && sanitizeHtml(value).length < min) {
         let message = MessagesUtil.models.validations.length.min.replace('XXX', min);
         return next(_.startCase(fieldName) + message);
       }
 
       let max = values.max;
-      if(max && value.length > max) {
+      if(max && sanitizeHtml(value).length > max) {
         let message = MessagesUtil.models.validations.length.max.replace('XXX', max);
         return next(_.startCase(fieldName) + message);
       }
