b2caa5b2aee27a2e20309901d10b771ba7a064fd
Sergey Ryabkov <s.ryabkov@aimprosoft.com>
2017-11-22 12:28:00 +0200

    TA1915: - move magic number to const;

--- public/js/ngApp/modules/fileUploader/fileUploader.js
+++ public/js/ngApp/modules/fileUploader/fileUploader.js
@@ -8,6 +8,9 @@
 
     var requestError = 'Request failed';
     var fileUploaderService = {};
+    // TODO: might be it needs move this const into some storage for cross-project variables
+    // this const is used in chat project: https://github.com/DiatomEnterprises/klzii_chat/blob/master/lib/klzii_chat/endpoint.ex
+    var MAX_FILE_SIZE = 5000000;
 
     fileUploaderService.token = null;
     fileUploaderService.upload = upload;
@@ -195,7 +198,7 @@
     function switchErrors(deferred, error) {
       switch (true) {
         case error.status == -1:
-          if (error.config.file.size > 5000000) {
+          if (error.config.file.size > MAX_FILE_SIZE) {
             deferred.reject("File is too big");
           } else {
             // it would be nice to check "error.status === 504", but status is still "-1"
